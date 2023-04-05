b0d1dba6359c79e6e0d29a8115b53894f9a38f29
jbryan83 <james.bryan.home@gmail.com>
2023-04-05 14:50:54 -0500

    Antialiased text

--- src/app/layout.js
+++ src/app/layout.js
@@ -8,7 +8,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang='en' className=''>
+    <html lang='en' className='antialiased'>
       <body className='bg-slate-200 dark:bg-slate-900 text-slate-800 dark:text-slate-300'>
         {children}
       </body>
