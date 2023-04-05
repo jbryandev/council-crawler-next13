64220b6e2bc496b299d931c04aefb64e2d1e40ac
jbryan83 <james.bryan.home@gmail.com>
2023-04-05 14:07:20 -0500

    Menu visual improvements

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -3,7 +3,7 @@ export default function Menu({ open, active, children }) {
     <menu
       className={`${
         open && active
-          ? 'flex flex-col absolute top-16 p-3 mt-3 rounded-lg border-solid border-2 border-slate-400 dark:border-slate-700 font-medium bg-slate-200 dark:bg-slate-900'
+          ? 'z-20 flex flex-col absolute top-16 p-3 mt-3 rounded-lg drop-shadow-lg font-medium bg-slate-50 dark:bg-slate-900'
           : 'hidden xl:grid gap-3'
       }`}
     >
