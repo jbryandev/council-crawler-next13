4e4bf5c97500c3b2d9ca94bc023aae3cbaa0dafe
jbryan83 <james.bryan.home@gmail.com>
2023-04-05 14:56:24 -0500

    Visual enhancements

--- src/components/AgendaViewer.js
+++ src/components/AgendaViewer.js
@@ -4,14 +4,14 @@ export default function AgendaViewer({ agenda }) {
   const agency = agencies.find((agency) => agency.id === agenda?.agency);
 
   return (
-    <main className='p-5 bg-slate-50 dark:bg-slate-800 rounded-lg'>
+    <main className='p-5 bg-slate-50 dark:bg-slate-800 rounded-lg drop-shadow-lg'>
       <h1 className='text-3xl font-semibold mt-5 mb-2'>
         {new Date(agenda.date).toLocaleDateString('en-us', {
           dateStyle: 'long',
         })}
       </h1>
 
-      <h2 className='text-lg mb-10'>{agency.name}</h2>
+      <h2 className='text-lg mb-10 opacity-75'>{agency.name}</h2>
 
       <p className='whitespace-pre-line text-justify max-w-prose'>
         {agenda.content}
