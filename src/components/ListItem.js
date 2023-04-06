2e2073816fed04f76f1153d27f15b9f46612784f
jbryan83 <james.bryan.home@gmail.com>
2023-04-05 21:28:11 -0500

    Navigator xl:hover:dark fix

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -14,7 +14,7 @@ export default function ListItem({
       className={`flex justify-between items-center mt-3 first:mt-0 p-5 rounded-lg cursor-pointer font-medium ${
         active
           ? 'bg-blue-700 hover:bg-blue-700/95 dark:bg-blue-800 dark:hover:bg-blue-800/90 text-slate-50'
-          : 'hover:bg-slate-200 xl:hover:bg-slate-300 dark:hover:bg-slate-800'
+          : 'hover:bg-slate-200 xl:hover:bg-slate-300 dark:hover:bg-slate-800 xl:dark:hover:bg-slate-800'
       } `}
       onClick={onClick}
     >
