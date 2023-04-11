27f2efc70f4054bded8edd2e9462573cc4506274
jbryan83 <james.bryan.home@gmail.com>
2023-04-11 14:43:39 -0500

    Fix new line display of agenda content

--- src/app/page.js
+++ src/app/page.js
@@ -4,5 +4,11 @@ import prisma from '@/utils/prisma';
 export default async function Page() {
   const agencies = await prisma.agency.findMany();
   const agendas = await prisma.agenda.findMany();
-  return <AgendaViewer agencies={agencies} agendas={agendas} />;
+  const agendasWithNewLines = agendas.map((agenda) => {
+    return {
+      ...agenda,
+      content: agenda.content.replace(/\\n/g, '\n'),
+    };
+  });
+  return <AgendaViewer agencies={agencies} agendas={agendasWithNewLines} />;
 }
