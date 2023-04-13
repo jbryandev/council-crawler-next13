ec370ede82b1a42622072af054a3ee41a0739e41
jbryan83 <james.bryan.home@gmail.com>
2023-04-13 18:34:56 -0500

    Improvements to smooth animation on ios

--- src/components/HamburgerMenu.js
+++ src/components/HamburgerMenu.js
@@ -5,28 +5,25 @@ const HamburgerMenu = forwardRef(function HamburgerMenu(
   { active, onClick },
   ref
 ) {
-  const hamburgerLine = `h-[3px] w-7 my-[3px] transition ease transform duration-300 ${
-    active ? 'bg-slate-600 dark:bg-slate-300' : 'bg-slate-50'
-  }`;
-
   return (
     <Button
-      styles={`lg:hidden flex-col ${
-        active &&
-        '!bg-slate-300 hover:!bg-slate-400/40 dark:!bg-slate-800 dark:hover:!bg-slate-700/60'
-      }`}
-      active={!active}
+      styles={`lg:hidden flex-col !bg-blue-700 ${active && '!bg-blue-700'}`}
+      active={active}
       onClick={onClick}
       ref={ref}
     >
       <div
-        className={`${hamburgerLine} ${
+        className={`h-[3px] w-7 my-[3px] transition ease transform duration-500 bg-slate-50 ${
           active && 'rotate-45 translate-y-[9px]'
         }`}
       />
-      <div className={`${hamburgerLine} ${active && 'opacity-0'}`} />
       <div
-        className={`${hamburgerLine} ${
+        className={`h-[3px] w-7 my-[3px] transition ease transform duration-500 bg-slate-50 ${
+          active && 'opacity-0'
+        }`}
+      />
+      <div
+        className={`h-[3px] w-7 my-[3px] transition ease transform duration-500 bg-slate-50 ${
           active && '-rotate-45 -translate-y-[9px]'
         }`}
       />
