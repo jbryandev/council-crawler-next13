466ee54f41b7f62baefc46cd66706b60db50ad98
jbryan83 <james.bryan.home@gmail.com>
2023-04-14 20:58:13 -0500

    Remove Button component

--- src/components/HamburgerMenu.js
+++ src/components/HamburgerMenu.js
@@ -1,33 +1,32 @@
 import { forwardRef } from 'react';
-import Button from './Button';
 
 const HamburgerMenu = forwardRef(function HamburgerMenu(
   { active, onClick },
   ref
 ) {
   return (
-    <Button
-      styles='lg:hidden flex-col !bg-blue-700 dark:!bg-blue-800 hover:!bg-blue-600 dark:hover:!bg-blue-700'
+    <button
+      className='lg:hidden justify-center p-5 rounded-lg cursor-pointer bg-blue-700 dark:bg-blue-800 hover:bg-blue-600 dark:hover:bg-blue-700'
       active={active}
       onClick={onClick}
       ref={ref}
     >
       <div
-        className={`h-[3px] w-7 my-[3px] duration-500 bg-slate-50 ${
-          active && 'rotate-45 translate-y-[9px]'
+        className={`h-[3px] w-7 my-1 duration-500 bg-slate-50 ${
+          active && 'rotate-45 translate-y-[7px]'
         }`}
       />
       <div
-        className={`h-[3px] w-7 my-[3px] duration-500 bg-slate-50 ${
+        className={`h-[3px] w-7 my-1 duration-500 bg-slate-50 ${
           active && 'opacity-0'
         }`}
       />
       <div
-        className={`h-[3px] w-7 my-[3px] duration-500 bg-slate-50 ${
-          active && '-rotate-45 -translate-y-[9px]'
+        className={`h-[3px] w-7 my-1 duration-500 bg-slate-50 ${
+          active && '-rotate-45 -translate-y-[7px]'
         }`}
       />
-    </Button>
+    </button>
   );
 });
 
