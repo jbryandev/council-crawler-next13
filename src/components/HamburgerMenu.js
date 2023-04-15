b561bb68f2e31079257f23f1483aee91c7429642
jbryan83 <james.bryan.home@gmail.com>
2023-04-14 19:58:13 -0500

    Hamburger menu visual enhancements

--- src/components/HamburgerMenu.js
+++ src/components/HamburgerMenu.js
@@ -7,25 +7,23 @@ const HamburgerMenu = forwardRef(function HamburgerMenu(
 ) {
   return (
     <Button
-      styles={`lg:hidden flex-col !bg-blue-700 ${
-        active && '!bg-slate-400 dark:!bg-slate-800'
-      }`}
+      styles='lg:hidden flex-col !bg-blue-700 dark:!bg-blue-800 hover:!bg-blue-600 dark:hover:!bg-blue-700'
       active={active}
       onClick={onClick}
       ref={ref}
     >
       <div
-        className={`h-[3px] w-7 my-[3px] transition ease transform duration-500 bg-slate-50 ${
+        className={`h-[3px] w-7 my-[3px] duration-500 bg-slate-50 ${
           active && 'rotate-45 translate-y-[9px]'
         }`}
       />
       <div
-        className={`h-[3px] w-7 my-[3px] transition ease transform duration-500 bg-slate-50 ${
+        className={`h-[3px] w-7 my-[3px] duration-500 bg-slate-50 ${
           active && 'opacity-0'
         }`}
       />
       <div
-        className={`h-[3px] w-7 my-[3px] transition ease transform duration-500 bg-slate-50 ${
+        className={`h-[3px] w-7 my-[3px] duration-500 bg-slate-50 ${
           active && '-rotate-45 -translate-y-[9px]'
         }`}
       />
