1c7ea559fc2690064a82f961cd4a6b24b2513174
jbryan83 <james.bryan.home@gmail.com>
2023-05-01 15:24:38 -0500

    Modified page to use dummy data

--- src/app/page.js
+++ src/app/page.js
@@ -1,16 +1,17 @@
 import AgendaViewer from '@/components/AgendaViewer';
 import prisma from '@/utils/prisma';
+import { agencies, agendas } from '@/utils/data';
 
 export default async function Page() {
-  const agencies = await prisma.agency.findMany();
-  const agendas = (await prisma.agenda.findMany()).map((agenda) => {
-    return {
-      ...agenda,
-      date: new Date(agenda.date).toLocaleDateString('en-us', {
-        dateStyle: 'long',
-      }),
-      content: agenda.content.replace(/\\n/g, '\n'),
-    };
-  });
+  // const agencies = await prisma.agency.findMany();
+  // const agendas = (await prisma.agenda.findMany()).map((agenda) => {
+  //   return {
+  //     ...agenda,
+  //     date: new Date(agenda.date).toLocaleDateString('en-us', {
+  //       dateStyle: 'long',
+  //     }),
+  //     content: agenda.content.replace(/\\n/g, '\n'),
+  //   };
+  // });
   return <AgendaViewer agencies={agencies} agendas={agendas} />;
 }
