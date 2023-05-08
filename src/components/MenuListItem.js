5bea4dff5b5f5ed90fc8c24321bb1d03697e2ce3
jbryan83 <james.bryan.home@gmail.com>
2023-05-08 13:49:20 -0500

    MenuListItem add w-full to style

--- src/components/MenuListItem.js
+++ src/components/MenuListItem.js
@@ -8,7 +8,7 @@ const MenuListItem = forwardRef(function MenuListItem(
     <button
       className={`group flex ${
         iconLeft || iconRight ? 'justify-between' : 'justify-center'
-      } items-center p-5 rounded-lg cursor-pointer font-medium text-left ${
+      } items-center p-5 w-full rounded-lg cursor-pointer font-medium text-left ${
         active
           ? 'bg-blue-700 hover:bg-blue-600 dark:bg-blue-800 dark:hover:bg-blue-700 text-slate-50'
           : 'bg-slate-50 hover:bg-slate-200 dark:bg-slate-900 dark:hover:bg-slate-800 lg:bg-slate-200 lg:hover:bg-slate-300 lg:dark:bg-slate-900 lg:dark:hover:bg-slate-800'
