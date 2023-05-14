6995c97087cacbf2697295095ba0ea6d6378ff25
jbryan83 <james.bryan.home@gmail.com>
2023-05-14 13:49:02 -0500

    MenuListItem full width

--- src/components/MenuListItem.js
+++ src/components/MenuListItem.js
@@ -6,9 +6,9 @@ const MenuListItem = forwardRef(function MenuListItem(
 ) {
   return (
     <button
-      className={`group flex ${
+      className={`group flex w-full ${
         iconLeft || iconRight ? 'justify-between' : 'justify-center'
-      } items-center p-5 w-full rounded-lg cursor-pointer font-medium text-left ${
+      } items-center p-5 rounded-lg cursor-pointer font-medium text-left ${
         active
           ? 'bg-blue-700 hover:bg-blue-600 dark:bg-blue-800 dark:hover:bg-blue-700 text-slate-50'
           : 'bg-slate-50 hover:bg-slate-200 dark:bg-slate-900 dark:hover:bg-slate-800 lg:bg-slate-200 lg:hover:bg-slate-300 lg:dark:bg-slate-900 lg:dark:hover:bg-slate-800'
