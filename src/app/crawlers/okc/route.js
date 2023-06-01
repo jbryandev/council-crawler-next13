814fa7111b5845cff3fa79604e47fe91d8e40e80
jbryan83 <james.bryan.home@gmail.com>
2023-05-31 21:10:59 -0500

    Add try block to okc crawler route

--- src/app/crawlers/okc/route.js
+++ src/app/crawlers/okc/route.js
@@ -9,63 +9,67 @@ const meetingsUrl = `${mainUrl}api/v2/PublicPortal/ListUpcomingMeetings?_=166447
 const agendaUrl = `${mainUrl}Portal/Meeting?meetingTemplateId=`;
 
 export async function crawl() {
-  // Get list of upcoming meetings
-  const meetings = await fetch(meetingsUrl).then((res) => res.json());
+  try {
+    // Get list of upcoming meetings
+    const meetings = await fetch(meetingsUrl).then((res) => res.json());
 
-  // Search for a meeting matching the given committee Id
-  const meeting = meetings.find((meeting) => {
-    return meeting.committeeId === 2; // OCWUT
-  });
-  if (!meeting) {
-    return NextResponse.json({ message: 'No upcoming meetings found.' });
-  }
+    // Search for a meeting matching the given committee Id
+    const meeting = meetings.find((meeting) => {
+      return meeting.committeeId === 2; // OCWUT
+    });
+    if (!meeting) {
+      return NextResponse.json({ message: 'No upcoming meetings found.' });
+    }
 
-  // Locate the HTML agenda for the upcoming meeting
-  const agenda = meeting.documentList.find((document) => {
-    return document.templateName.toLowerCase().includes('html agenda');
-  });
-  if (!agenda) {
-    return NextResponse.json({ message: 'No HTML agenda could be found.' });
-  }
-  const agendaPath = `${agendaUrl}${agenda.templateId}`;
+    // Locate the HTML agenda for the upcoming meeting
+    const agenda = meeting.documentList.find((document) => {
+      return document.templateName.toLowerCase().includes('html agenda');
+    });
+    if (!agenda) {
+      return NextResponse.json({ message: 'No HTML agenda could be found.' });
+    }
+    const agendaPath = `${agendaUrl}${agenda.templateId}`;
 
-  // Make sure the agenda doesn't already exist in the db
-  const existingAgenda = await prisma.agenda.findUnique({
-    where: { url: agendaPath },
-  });
-  if (existingAgenda) {
-    return NextResponse.json({ message: 'Meeting already exists.' });
-  }
+    // Make sure the agenda doesn't already exist in the db
+    const existingAgenda = await prisma.agenda.findUnique({
+      where: { url: agendaPath },
+    });
+    if (existingAgenda) {
+      return NextResponse.json({ message: 'Meeting already exists.' });
+    }
 
-  // Fetch the agenda HTML
-  const agendaHtml = await fetch(agendaPath).then((res) => res.text());
-  const agendaText = convert(agendaHtml, {
-    wordwrap: 130,
-    selectors: [
-      {
-        selector: 'a',
-        options: {
-          ignoreHref: true,
+    // Fetch the agenda HTML
+    const agendaHtml = await fetch(agendaPath).then((res) => res.text());
+    const agendaText = convert(agendaHtml, {
+      wordwrap: 130,
+      selectors: [
+        {
+          selector: 'a',
+          options: {
+            ignoreHref: true,
+          },
+        },
+      ],
+    });
+
+    // Add the agenda to the db
+    const newAgenda = await prisma.agenda.create({
+      data: {
+        date: new Date(meeting.date).toISOString(),
+        title: meeting.title,
+        url: agendaPath,
+        content: agendaText,
+        agency: {
+          connect: {
+            id: '63326daba3e76095e0426add', // OCWUT
+          },
         },
       },
-    ],
-  });
-
-  // Add the agenda to the db
-  const newAgenda = await prisma.agenda.create({
-    data: {
-      date: new Date(meeting.date).toISOString(),
-      title: meeting.title,
-      url: agendaPath,
-      content: agendaText,
-      agency: {
-        connect: {
-          id: '63326daba3e76095e0426add', // OCWUT
-        },
-      },
-    },
-  });
-  return NextResponse.json({ newAgenda });
+    });
+    return NextResponse.json({ newAgenda });
+  } catch (error) {
+    return NextResponse.json({ error });
+  }
 }
 
 export async function GET() {
